34415ebfd36c6630024861353d483c074ea8ef0c
Miguel Hernandez <phyzix5761@gmail.com>
2017-01-09 14:12:05 -0500

    Updated example
    Example is now a bit more useful

--- example.js
+++ example.js
@@ -10,7 +10,10 @@ config = [
 ];
 
 function doSomethingWithResponse(response) {
-	console.log(response);
+	for(let i = 0; i < response.length; i++) {
+		let api = response[i];
+		console.log(api.name, "status:", api.status);
+	}
 };
 
 let apiDoc = new ApiDoctor();
